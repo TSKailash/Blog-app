61359f037eee5ccb693b4f139755ff576ac33c6e
TSKailash <tskailash20@gmail.com>
2025-03-27 13:59:29 +0530

    feat

---
+++ backend/routes/UserRoutes.js
@@ -0,0 +1,43 @@
+const express=require('express')
+const router=express.Router()
+const User=require('../models/UserModel')
+
+router.post('/signup', async(req, res)=>{
+    try{
+        const {username, email, password}=req.body;
+        const existingUser=await User.findOne({email})
+        if(existingUser){
+            return res.status(400).send({message:"User already exists"})
+        }
+        const user=new User({username, email, password})
+        await user.save()
+        return res.status(200).send({message:"Success"})
+    }
+    catch(err){
+        return res.status(400).send({message:err.message})
+    }
+})
+
+router.post('/login', async(req, res)=>{
+    try{
+        const {email, password}=req.body;
+        const existingUser=await User.findOne({email})
+        if(existingUser){
+            if(password===existingUser.password){
+                return res.status(200).send({message:"success"})
+            }
+            else{
+                return res.status(200).send({message:"wrong_p"})
+            }
+        }
+        else{
+            return res.status(200).send({message:"User not_found"})
+        }
+    }
+    catch(err){
+        console.log(err)
+        return res.status(401).send({message:err.message})
+    }
+})
+
+module.exports=router
--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,14 @@ const express=require('express')
 const mongoose=require('mongoose')
 const cors=require('cors')
 const User=require('./models/UserModel')
-
+require('dotenv').config();
+const userRoutes=require('./routes/UserRoutes')
 
 const app=express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://localhost:27017/blog")
+mongoose.connect(process.env.MONGO_URL)
         .then(()=>{console.log("MongoDB Connected")})
         .catch((err)=>{console.log(err)})
 
@@ -16,23 +17,26 @@ app.get("/",(req,res)=>{
     res.send("HEllo");
 })
 
-app.post("/addUser", async(req, res)=>{
-    try{
-        const {username, email, password}=req.body
-        const exist=await User.findOne({email})
-        if(exist){
-             return res.status(400).json({message: "Already created"})
-        } 
-        const user=new User({username, email, password})
-        await user.save()
-        return res.status(200).json({message: "User created"})
-    }
-    catch(err){
-        console.log(err);
-        return res.status(404).json({error: err.message})
-    }
+// app.post("/addUser", async(req, res)=>{
+//     try{
+//         const {username, email, password}=req.body
+//         const exist=await User.findOne({email})
+//         if(exist){
+//              return res.status(400).json({message: "Already created"})
+//         } 
+//         const user=new User({username, email, password})
+//         await user.save()
+//         return res.status(200).json({message: "User created"})
+//     }
+//     catch(err){
+//         console.log(err);
+//         return res.status(404).json({error: err.message})
+//     }
 
-})
+// })
+
+app.use('/', userRoutes)
+
 app.listen(3000, ()=>{
     console.log("App running")
 })
